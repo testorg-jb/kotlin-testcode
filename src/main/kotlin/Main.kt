450e7006f14d56020f14519e2e5575f56438ccd9
Johann Beleites <johann.beleites@sonarsource.com>
2022-06-28 11:09:36 +0200

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,4 +4,56 @@ fun main(args: Array<String>) {
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+
+    SomeClassWithManyConstructorArgs(
+        var0 = "some var 0",
+        var1 = "some var 1",
+        var2 = "some var 2",
+        var3 = "some var 3",
+        var4 = "some var 4",
+        var5 = "some var 5",
+        var6 = "some var 6",
+        var7 = "some var 7",
+        var8 = "some var 8",
+        var9 = "some var 9",
+    )
+
+    SomeClassWithManyConstructorArgs(
+        var0 = "some var 0",
+        var1 = "some var 1",
+        var2 = "some var 2",
+        var3 = "some var 3",
+        var4 = "some var 4",
+        var5 = "some var 5",
+        var6 = "some var 6",
+        var7 = "some var 7",
+        var8 = "some var 8",
+        var9 = "some var 9",
+    )
+
+    SomeClassWithManyConstructorArgs(
+         "without named args 0",
+         "without named args 1",
+         "without named args 2",
+         "without named args 3",
+         "without named args 4",
+         "without named args 5",
+         "without named args 6",
+         "without named args 7",
+         "without named args 8",
+         "without named args 9",
+    )
+
+    SomeClassWithManyConstructorArgs(
+         "without named args 0",
+         "without named args 1",
+         "without named args 2",
+         "without named args 3",
+         "without named args 4",
+         "without named args 5",
+         "without named args 6",
+         "without named args 7",
+         "without named args 8",
+         "without named args 9",
+    )
 }
---
+++ src/main/kotlin/SomeClassWithManyConstructorArgs.kt
@@ -0,0 +1,12 @@
+class SomeClassWithManyConstructorArgs(
+    val var0: String,
+    val var1: String,
+    val var2: String,
+    val var3: String,
+    val var4: String,
+    val var5: String,
+    val var6: String,
+    val var7: String,
+    val var8: String,
+    val var9: String,
+)
